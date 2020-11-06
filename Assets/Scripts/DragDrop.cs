ef0341b04b6c809418740e304b3ce2e09a13ea8d
JD Calvelli <58123790+jdcalvelli@users.noreply.github.com>
2020-11-05 21:35:15 -0500

    finish drag/drop and add drop slot scripts

--- Assets/Scripts/DragDrop.cs
+++ Assets/Scripts/DragDrop.cs
@@ -7,27 +7,36 @@ public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, I
 {
 
     private RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
 
     private void Awake() 
     {
-        rectTransform = GetComponent<RectTransform>();    
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
         Debug.Log("OnPointerDown");
     }
+
     public void OnBeginDrag(PointerEventData eventData)
     {
         Debug.Log("OnBeginDrag");
+        canvasGroup.blocksRaycasts = false;
     }
-    public void OnEndDrag(PointerEventData eventData)
-    {
-        Debug.Log("OnEndDrag");
-    }
+
     public void OnDrag(PointerEventData eventData)
     {
         Debug.Log("OnDrag");
         rectTransform.anchoredPosition += eventData.delta;
+        canvasGroup.alpha = 0.6f;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEndDrag");
+        canvasGroup.blocksRaycasts = true;
+        canvasGroup.alpha = 1f;
     }
 }
