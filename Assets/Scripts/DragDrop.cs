ad11ef987169a30dc38b8d47d757c615460deaca
JD Calvelli <58123790+jdcalvelli@users.noreply.github.com>
2020-11-05 21:22:43 -0500

    add temp objects and basic drag and drop script

---
+++ Assets/Scripts/DragDrop.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+
+    private RectTransform rectTransform;
+
+    private void Awake() 
+    {
+        rectTransform = GetComponent<RectTransform>();    
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("OnPointerDown");
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnBeginDrag");
+    }
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEndDrag");
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnDrag");
+        rectTransform.anchoredPosition += eventData.delta;
+    }
+}
