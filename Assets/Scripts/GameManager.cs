63ae2528f37015a4e92efe73e7be0afc1a700ac8
JD Calvelli <58123790+jdcalvelli@users.noreply.github.com>
2020-11-09 16:42:42 -0500

    add game manager, expository text on startup

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public List<StoryElement> expositionElements;
+
+    public string GameState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameState = "Part 1";
+        Invoke("StartExposition", 0.5f); //like a coroutine lmao
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void StartExposition()
+    {
+        TriggerExposition(0);
+    }
+
+    private void TriggerExposition(int exposition)
+    {
+        expositionElements[exposition].TriggerDialogue();
+    }
+}
