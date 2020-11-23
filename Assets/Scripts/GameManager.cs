0f711a64fb8c968d63b200751242fdcf9c950381
JD Calvelli <58123790+jdcalvelli@users.noreply.github.com>
2020-11-22 22:30:34 -0500

    add title overlay

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject startingOverlay;
 
     public List<StoryElement> expositionElements;
 
@@ -16,30 +19,45 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GameState = 1;
-        Invoke("StartExposition", 0.5f); //like a coroutine lmao
+        GameState = 0;
+        TitleFadeOut();
+        //Invoke("StartExposition", 0.5f); //like a coroutine lmao
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (GameState == 1 && ChangeState == false)
+        {
+            TriggerExposition(0);
+            ChangeState = true;
+        }
 
-        if (GameState == 2 && ChangeState == false)
+        if (GameState == 2 && ChangeState == true)
         {
             TriggerExposition(1);
-            ChangeState = true;
-        }
-        else if (GameState == 3 && ChangeState == true)
-        {
-            TriggerExposition(2);
             ChangeState = false;
         }
-        else if(GameState == 4 && ChangeState == false)
+        else if (GameState == 3 && ChangeState == false)
         {
-            TriggerExposition(3);
+            TriggerExposition(2);
             ChangeState = true;
         }
+        else if(GameState == 4 && ChangeState == true)
+        {
+            TriggerExposition(3);
+            ChangeState = false;
+        }
 
+    }
+
+    private void TitleFadeOut()
+    {
+        Sequence mySequence = DOTween.Sequence();
+        mySequence.AppendInterval(3f);
+        mySequence.Append(startingOverlay.GetComponent<CanvasGroup>().DOFade(0f, 1f));
+        mySequence.AppendCallback(()=> GameState++);
+        mySequence.AppendCallback(()=> startingOverlay.SetActive(false));
     }
 
     private void StartExposition()
