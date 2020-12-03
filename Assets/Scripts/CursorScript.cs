dfd6c73a8e55c9c725de4c6ed85ca42c33b90f88
JD Calvelli <58123790+jdcalvelli@users.noreply.github.com>
2020-12-03 13:56:30 -0500

    add final cursor image using cursor script

---
+++ Assets/Scripts/CursorScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorScript : MonoBehaviour
+{
+    public Texture2D cursorTexture;
+
+    void Start()
+    {
+        Cursor.SetCursor(cursorTexture, new Vector2(10f, 0f), CursorMode.ForceSoftware);    
+    }
+
+}
